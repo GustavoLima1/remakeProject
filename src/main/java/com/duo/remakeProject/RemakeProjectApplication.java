702ede8910c2e265ac79f86ac3bdbe8c823e7d46
Gustavo Lima <gustavo1.l@hotmail.com>
2021-05-12 19:54:24 -0300

    first commit

---
+++ src/main/java/com/duo/remakeProject/RemakeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.duo.remakeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemakeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemakeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duo/remakeProject/RemakeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duo.remakeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemakeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
